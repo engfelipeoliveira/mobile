a9be273dc0a50c6e88e49f6820208b7b3898eb2c
Felipe <cvfelipeoliveira@gmail.com>
2020-02-21 23:30:05 -0500

    mobile

--- js/script.js
+++ js/script.js
@@ -2,93 +2,70 @@ var dataSetFavorites = [];
 
 $(document).ready(function() {
 	
-	// armazenamento das noticias em array para evitar calls desnecessarios
+	// set arrays of the news by suject type
 	getDate();
 	getNews('sport');
 	getNews('business');
 	getNews('financial');
+	
+	// add news into array of the favorites
+	$(".add-favorite").on("click", function(e) {
+		var idLine = $(this).attr("id");
+		var newsExists = false;
+		$.each(dataSetFavorites, function() {
+			if(idLine == this.idLine){
+				newsExists = true;
+				return false;
+			}	
+		});	
 		
-	// escoder e exibir o conteudo conforme botoes de acao no menu
-	$("#menu-home").on("click", function(e) {
-		getDate();
-		$("#div-home").show();
-		$("#div-sport").hide();
-		$("#div-business").hide();
-		$("#div-financial").hide();		
-	});
-
-	$("#menu-sport").on("click", function(e) {
-		$("#div-home").hide();
-		$("#div-sport").show();
-		$("#div-business").hide();
-		$("#div-financial").hide();
+		if(!newsExists){
+			var publishedAt = $("#publishedAt" + idLine).html();
+			var description = $("#description" + idLine).html();
+			dataSetFavorites.push({idLine, publishedAt, description});
+			setFavorite();			
+		}	
 	});
 	
-	$("#menu-business").on("click", function(e) {
-		$("#div-home").hide();
-		$("#div-sport").hide();
-		$("#div-business").show();
-		$("#div-financial").hide();
-	});
-
-	$("#menu-financial").on("click", function(e) {
-		$("#div-home").hide();
-		$("#div-sport").hide();
-		$("#div-business").hide();
-		$("#div-financial").show();
-	});
-	
-	$(".btn-fav").on("click", function(e) {
-		var lineNews = $(this).attr("id");
-		var table = lineNews.split('-')[0];
-		var tr = lineNews.split('-')[1];
-		
-		var publishedAt = $("#dataTable-" + table)[0].rows[tr].cells[0].innerText;
-		var description = $("#dataTable-" + table)[0].rows[tr].cells[1].innerText;
-
-		dataSetFavorites.push([publishedAt, description, ""]);						
-	});
-	
-	
-		
 });
 
+// get moment date and shows into home page
 function getDate() {
 	$('#dataTable-home').html(new Date());	
 }
 
-//metodo para obter as noticias do site news api atraves de um call GET
-function getNews(type) {
+// call rest-service from website news-api
+function getNews(type) {	
 	var url = 'http://newsapi.org/v2/everything?q='+type+'&apiKey=d77b40b94e714053b0e20391cad1954b';
+	var count = 0;
 	$.ajax({
 		url : url,
 		type : "GET",
 		async : false, 
 		success : (function(data, status, jqXhr) {
-			var dataSet = [];
-			let news;
-			var count = 0;
 			$.each(data.articles, function() {
-				news = {publishedAt : this.publishedAt, description : this.description};
-				dataSet.push([new Date(this.publishedAt).toLocaleDateString("en-IE"), this.description, "<a class='btn-fav' id='"+type+"-"+this.publishedAt+"' href='#'><i class='fas fa-star'></i></a>"]);				
-			});
-			setDataTable(dataSet, type);			
+				var id = type + count;
+				var line = "<tr><td id='publishedAt"+id+"'>"+ new Date(this.publishedAt).toLocaleDateString("en-IE") +"</td>" 
+				+ "<td id='description"+id+"'>"+ this.description +"</td>"
+				+ "<td><a href='#' id='"+id+"' class='add-favorite'>add</a></td></tr>";
+				setDataTable(line, type);
+				count++; 
+			});			
 		})
 	});
 }
 
-// metodo para transformar o json em um datatable
-function setDataTable(dataSet, el) {
-	$('#dataTable-' + el).DataTable( {
-		"searching": true,
-		"info": true,
-		"lengthChange": true,
-		rightColumns: 1,
-		data: dataSet,
-		columns: [
-			{ title: "Published" },
-			{ title: "Description" },
-			{ title: "Favorites" }
-		]
-	});	
+// parse json to datatable
+function setDataTable(line, type) {
+	$('#tbody-' + type).append(line);	
+}
+
+// parse array to favorite datatable
+function setFavorite() {
+	$('#tbody-favorites').empty();
+	$.each(dataSetFavorites, function() {
+		var line = "<tr><td>"+ this.publishedAt +"</td>" 
+		+ "<td>"+ this.description +"</td>"
+		setDataTable(line, 'favorites');
+	});		
 }
